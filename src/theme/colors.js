6a2a226db55331befcb58c53129c2dfb0e7a74aa
lassandra85 <lassandra85@gmail.com>
2026-05-20 23:48:25 +0200

    new

---
+++ src/theme/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+primary: "#4A90E2",
+secondary: "#FF7A59",
+textDark: "#222",
+textLight: "#fff",
+gray: "#ccc",
+bg: "#f8f8f8",
+};
---
+++ src/theme/sizes.js
@@ -0,0 +1,16 @@
+export const SIZES = {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32,
+/* 
+     xs: 4,
+  sm: 8,
+  md: 12,
+  lg: 16,
+  xl: 20,
+  xxl: 24,
+  xxxl: 28,
+  huge: 32, */
+};
