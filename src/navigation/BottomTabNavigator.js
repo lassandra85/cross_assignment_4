317dd7bff6059690779f5fe9bacda94dfaa767c8
lassandra85 <lassandra85@gmail.com>
2026-06-16 22:59:58 +0200

    BottomTabNavigator.js

---
+++ src/navigation/BottomTabNavigator.js
@@ -0,0 +1,10 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeStackNavigator from './HomeStackNavigator';
+
+import FavoritesScreen from '../screens/FavoritesScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+import { SCREENS } from '../constants/screens';
+
+import { Ionicons } from '@expo/vector-icons';
